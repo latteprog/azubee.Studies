e87405ea38dc962567300e1e6a52d8e6d2752e96
Luis Schweigard <luis.schweigard@maibornwolff.de>
2023-03-12 15:17:29 +0100

    Adding data and code for proficiency tracing

---
+++ kt_visualization.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from typing import Dict, List
+
+SKILL_ID = "it-network-plan-ipv4-addressing"
+
+datapoints: Dict[str, List[float]] = dict()
+
+for file in os.listdir("abzuege"):
+    data: pd.DataFrame = pd.read_csv(os.path.join("abzuege", file), sep=";")
+    mastery_values = data.where(data["SkillId"] == SKILL_ID).dropna()
+
+    for _, row in mastery_values.iterrows():
+        datapoints.setdefault(row["UserId"], list())
+        datapoints[str(row["UserId"])].append(float(row["Mastery"]))
+
+for index, key in enumerate(datapoints.keys()):
+    name = f"User {index}"
+    x = range(len(datapoints[key]))
+    y = datapoints[key]
+
+    plt.plot(x, y, label=name)
+
+plt.show()
