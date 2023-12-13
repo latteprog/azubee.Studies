07ceb50d6ac560e595694382576ab5a18da95ae9
Torben Soennecken <torben.soennecken@rootitup.de>
2023-12-13 19:29:19 +0100

    Changed labels of the diagram

--- pre_study.py
+++ pre_study.py
@@ -6,6 +6,7 @@ from util import calculate_cohends_d, render_boxplot, perform_test, plot_pre_pos
 # Exercise 3 : it-network-plan-vlan
 # Exercise 4 : it-network-plan-ipv4-static-routing
 
+
 # Users 1,3,5,7,9,11,13 it-network-plan-ipv4-static-routing
 # Users 2,4,6,8,10,12 it-network-plan-vlan
 def extract_entries(df: pd.DataFrame, was_trained: bool):
@@ -16,20 +17,32 @@ def extract_entries(df: pd.DataFrame, was_trained: bool):
 
     return eq[eq["User"].notna()]
 
+
 def prepare_data():
     data = pd.read_csv("preprocessed/pre_preprocessed.csv")
 
     for student in data["User"].unique():
         # Assuming student is defined
-        df = data.loc[data["User"] == student][["ExerciseSkill","PretestCorrectRel","PosttestCorrectRel"]].groupby("ExerciseSkill").mean()
+        df = (
+            data.loc[data["User"] == student][
+                ["ExerciseSkill", "PretestCorrectRel", "PosttestCorrectRel"]
+            ]
+            .groupby("ExerciseSkill")
+            .mean()
+        )
         df.reset_index(inplace=True)
 
-        plot_pre_post(df=df, filename=f"pre/barplots/scores_{int(student)}", title=f'Scores for User: {int(student)}')
+        plot_pre_post(
+            df=df,
+            filename=f"pre/barplots/scores_{int(student)}",
+            title=f"Scores for User: {int(student)}",
+        )
 
     trained = extract_entries(df=data, was_trained=True)
     not_trained = extract_entries(df=data, was_trained=False)
 
     return trained, not_trained
+
 
 def test_improvement_abs_skill(trained, untrained, is_graph_norm, norm_val=0.05):
     """
@@ -37,19 +50,33 @@ def test_improvement_abs_skill(trained, untrained, is_graph_norm, norm_val=0.05)
     The is_graph_norm is an indicator, if both distributions within the improvement_abs_skills.png file are a normal distribution.
     This decides the statistical test used for evaluation.
     """
-    trained_improvements = trained.groupby(["User", "ExerciseSkill"]).mean()["ImprovementAbsNormalizedScores"].to_numpy()
-    untrained_improvements = untrained.groupby(["User", "ExerciseSkill"]).mean()["ImprovementAbsNormalizedScores"].to_numpy()
+    trained_improvements = (
+        trained.groupby(["User", "ExerciseSkill"])
+        .mean()["ImprovementAbsNormalizedScores"]
+        .to_numpy()
+    )
+    untrained_improvements = (
+        untrained.groupby(["User", "ExerciseSkill"])
+        .mean()["ImprovementAbsNormalizedScores"]
+        .to_numpy()
+    )
 
     t_test_result = perform_test(
         is_related=True,
         a=trained_improvements,
         b=untrained_improvements,
-        a_name="Trained",b_name="Untrained", x_label="Score",
+        a_name="Trained",
+        b_name="Untrained",
+        x_label="Score",
         filename=f"pre/histograms/improvement_abs_skills",
-        is_graph_norm=is_graph_norm, norm_val=norm_val
+        is_graph_norm=is_graph_norm,
+        norm_val=norm_val,
     )
 
-    return t_test_result, calculate_cohends_d(trained_improvements, untrained_improvements)
+    return t_test_result, calculate_cohends_d(
+        trained_improvements, untrained_improvements
+    )
+
 
 def test_improvement_abs_exercise(trained, untrained, is_graph_norm, norm_val=0.05):
     """
@@ -64,14 +91,22 @@ def test_improvement_abs_exercise(trained, untrained, is_graph_norm, norm_val=0.
         is_related=True,
         a=trained_improvements,
         b=untrained_improvements,
-        a_name="Trained",b_name="Untrained", x_label="Score",
+        a_name="Trained",
+        b_name="Untrained",
+        x_label="Score",
         filename=f"pre/histograms/improvement_abs_exercises",
-        is_graph_norm=is_graph_norm, norm_val=norm_val
+        is_graph_norm=is_graph_norm,
+        norm_val=norm_val,
     )
 
-    return t_test_result, calculate_cohends_d(trained_improvements, untrained_improvements)
+    return t_test_result, calculate_cohends_d(
+        trained_improvements, untrained_improvements
+    )
 
-def test_improvement_normalized_change_skill(trained, untrained, is_graph_norm, norm_val=0.05):
+
+def test_improvement_normalized_change_skill(
+    trained, untrained, is_graph_norm, norm_val=0.05
+):
     """
     Function to calculate if, and how significant the learning improvement for the trained **skills** in terms of normalized change was, relative to the untrained skills.
     The is_graph_norm is an indicator, if both distributions within the improvement_normalized_change_skills.png file are a normal distribution.
@@ -87,14 +122,22 @@ def test_improvement_normalized_change_skill(trained, untrained, is_graph_norm, 
         is_related=True,
         a=trained_improvements,
         b=untrained_improvements,
-        a_name="Trained",b_name="Untrained", x_label="Score",
+        a_name="Trained",
+        b_name="Untrained",
+        x_label="Score",
         filename=f"pre/histograms/improvement_normalized_change_skills",
-        is_graph_norm=is_graph_norm, norm_val=norm_val
+        is_graph_norm=is_graph_norm,
+        norm_val=norm_val,
     )
 
-    return t_test_result, calculate_cohends_d(trained_improvements, untrained_improvements)
+    return t_test_result, calculate_cohends_d(
+        trained_improvements, untrained_improvements
+    )
 
-def test_improvement_normalized_change_exercise(trained, untrained, is_graph_norm, norm_val=0.05):
+
+def test_improvement_normalized_change_exercise(
+    trained, untrained, is_graph_norm, norm_val=0.05
+):
     """
     Function to calculate if, and how significant the learning improvement for the trained **exercises** in terms of normalized change was, relative to the untrained exercises.
     The is_graph_norm is an indicator, if both distributions within the improvement_normalized_change_exercises.png file are a normal distribution.
@@ -107,32 +150,71 @@ def test_improvement_normalized_change_exercise(trained, untrained, is_graph_nor
         is_related=True,
         a=trained_improvements,
         b=untrained_improvements,
-        a_name="Trained",b_name="Untrained", x_label="Score",
+        a_name="Trained",
+        b_name="Untrained",
+        x_label="Score",
         filename=f"pre/histograms/improvement_normalized_change_exercises",
-        is_graph_norm=is_graph_norm, norm_val=norm_val
+        is_graph_norm=is_graph_norm,
+        norm_val=norm_val,
     )
 
-    return t_test_result, calculate_cohends_d(trained_improvements, untrained_improvements)
+    return t_test_result, calculate_cohends_d(
+        trained_improvements, untrained_improvements
+    )
+
 
 trained, untrained = prepare_data()
 
-improv_t_res, improv_cohens = test_improvement_abs_skill(trained, untrained, is_graph_norm=False, norm_val=0.05)
-improv_exercise_t_res, improv_exercise_cohens = test_improvement_abs_exercise(trained, untrained, is_graph_norm=True, norm_val=0.05)
-normalized_t_res, normalized_cohens = test_improvement_normalized_change_skill(trained, untrained, is_graph_norm=True, norm_val=0.05)
-normalized_exercise_t_res, normalized_exercise_cohens = test_improvement_normalized_change_exercise(trained, untrained, is_graph_norm=False, norm_val=0.05)
+improv_t_res, improv_cohens = test_improvement_abs_skill(
+    trained, untrained, is_graph_norm=False, norm_val=0.05
+)
+improv_exercise_t_res, improv_exercise_cohens = test_improvement_abs_exercise(
+    trained, untrained, is_graph_norm=True, norm_val=0.05
+)
+normalized_t_res, normalized_cohens = test_improvement_normalized_change_skill(
+    trained, untrained, is_graph_norm=True, norm_val=0.05
+)
+(
+    normalized_exercise_t_res,
+    normalized_exercise_cohens,
+) = test_improvement_normalized_change_exercise(
+    trained, untrained, is_graph_norm=False, norm_val=0.05
+)
 
-data = pd.DataFrame({
-    'type' : ["improvement_abs_skill", "improvement_abs_exercise", "normalized_change_skill", "normalized_change_exercise"], 
-    't' : [improv_t_res.statistic, improv_exercise_t_res.statistic, normalized_t_res.statistic, normalized_exercise_t_res.statistic],
-    'p': [improv_t_res.pvalue, improv_exercise_t_res.pvalue, normalized_t_res.pvalue, normalized_exercise_t_res.pvalue],
-    'cohens': [improv_cohens, improv_exercise_cohens, normalized_cohens, normalized_exercise_cohens]
-})
+data = pd.DataFrame(
+    {
+        "type": [
+            "improvement_abs_skill",
+            "improvement_abs_exercise",
+            "normalized_change_skill",
+            "normalized_change_exercise",
+        ],
+        "t": [
+            improv_t_res.statistic,
+            improv_exercise_t_res.statistic,
+            normalized_t_res.statistic,
+            normalized_exercise_t_res.statistic,
+        ],
+        "p": [
+            improv_t_res.pvalue,
+            improv_exercise_t_res.pvalue,
+            normalized_t_res.pvalue,
+            normalized_exercise_t_res.pvalue,
+        ],
+        "cohens": [
+            improv_cohens,
+            improv_exercise_cohens,
+            normalized_cohens,
+            normalized_exercise_cohens,
+        ],
+    }
+)
 data.to_csv(f"results/pre_evaluation.csv", index=None)
 
 render_boxplot(
     trained.groupby(["User", "ExerciseSkill"]).mean()["NormalizedChange"],
     untrained.groupby(["User", "ExerciseSkill"]).mean()["NormalizedChange"],
     "pre/boxplot_normalized_change",
-    ["Trained Skills", "Untrained Skills"],
-    title="Normalized Learning Gain"
+    ["Trained (Faded) Skills", "Untrained (Unfaded) Skills"],
+    title="Normalized Learning Gain",
 )
